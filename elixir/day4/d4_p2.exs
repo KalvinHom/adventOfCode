59f99d9d7c569408e13b3f14eed9af641bc3087f
Kalvin Hom <kalvin.hom@gmail.com>
2019-12-04 12:48:14 -0800

    D4P2

--- elixir/day4/d4_p2.exs
+++ elixir/day4/d4_p2.exs
@@ -1,4 +1,4 @@
-defmodule D4P1 do
+defmodule D4P2 do
   def run!() do
     Enum.reduce(138_307..654_504, 0, fn number, count ->
       valid = never_decrease?(number) && has_repeat?(number)
@@ -18,12 +18,22 @@ defmodule D4P1 do
   end
 
   defp has_repeat?(number) do
-    number
-    |> Integer.digits()
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.any?(fn [x, y] -> x == y end)
+    chunks =
+      number
+      |> Integer.digits()
+      |> Enum.chunk_every(4, 1)
+
+    repeat? =
+      chunks
+      |> Enum.any?(fn [w, x, y | z] ->
+        w != x && x == y and [y] != z
+      end)
+
+    # base case for first 3 nums
+    [w, x, y, _] = List.first(chunks)
+    repeat? || (w == x && x != y)
   end
 end
 
-D4P1.run!()
+D4P2.run!()
 |> IO.puts()
