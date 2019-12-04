9b160fc847da08139a7b0d2abc3471e8b5b8ecb3
Kalvin Hom <kalvin.hom@gmail.com>
2019-12-04 12:14:07 -0800

    day 4 pt1

---
+++ elixir/day4/d4_p1.exs
@@ -0,0 +1,29 @@
+defmodule D4P1 do
+  def run!() do
+    Enum.reduce(138_307..654_504, 0, fn number, count ->
+      valid = never_decrease?(number) && has_repeat?(number)
+
+      case valid do
+        true -> count + 1
+        false -> count
+      end
+    end)
+  end
+
+  defp never_decrease?(number) do
+    number
+    |> Integer.digits()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [x, y] -> x <= y end)
+  end
+
+  defp has_repeat?(number) do
+    number
+    |> Integer.digits()
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.any?(fn [x, y] -> x == y end)
+  end
+end
+
+D4P1.run!()
+|> IO.inspect()
