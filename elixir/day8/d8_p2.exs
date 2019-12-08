083b0b0974e7daeb006e852ffbb87dcaefe4c229
Kalvin Hom <kalvin.hom@gmail.com>
2019-12-07 23:26:43 -0800

    playing around with benchee

--- elixir/day8/d8_p2.exs
+++ elixir/day8/d8_p2.exs
@@ -8,7 +8,6 @@ defmodule D8P2 do
       File.read!("day-8-input.txt")
       |> String.codepoints()
       |> Enum.chunk_every(size)
-      |> IO.inspect()
 
     colors =
       Enum.map(0..(size - 1), fn elem ->
@@ -19,10 +18,19 @@ defmodule D8P2 do
           end
         end)
       end)
-      |> IO.inspect()
+
+    # was curious how much converting to map saves in performance
+    # from not iterating list over and over.
+    # conclusion: not much with this small dataset, 4ms ish.
+    # color_map =
+    #   colors
+    #   |> Enum.with_index()
+    #   |> Enum.map(fn {k, v} -> {v, k} end)
+    #   |> Map.new()
 
     Enum.map(0..(height - 1), fn y ->
       Enum.map(0..(width - 1), fn x ->
+        # case Map.get(color_map, width * y + x) do
         case Enum.at(colors, width * y + x) do
           "2" -> IO.write(IO.ANSI.black_baground() <> " ")
           "0" -> IO.write(IO.ANSI.white_background() <> "0")
@@ -35,4 +43,8 @@ defmodule D8P2 do
   end
 end
 
-D8P2.run!()
+Benchee.run(%{
+  "d8 p2" => fn ->
+    D8P2.run!()
+  end
+})
